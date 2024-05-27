13d1ab3bee2dcb6b89ad27535b162c7b8a7ce6bf
haeisk <utkupkberkay@gmail.com>
2024-05-27 22:19:37 +0300

    word.v1

---
+++ Assets/scripts/Word.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Word
+{
+    public string word;
+
+    public Word(string   _word) {
+
+        word = _word;
+    }
+   
+}
